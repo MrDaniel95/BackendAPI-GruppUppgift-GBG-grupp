4298678cd624bf6c108b7b0e4053a3398e7c6437
kh-rasha <rasha.alkhateeb97@gmail.com>
2026-04-17 21:10:07 +0200

    implement login endpoint authentication flow

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,20 +1,34 @@
-
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.*;
+import com.example.demo.dto.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
-    @PostMapping("/login")
-    public String login(@RequestBody String body) {
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-        // TODO:
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+// TODO:
         // 1. Ta emot username + password
         // 2. Autentisera via Spring
         // 3. Generera JWT
         // 4. Returnera token
-
-        return "TODO JWT";
+        return ResponseEntity.ok("Login success (JWT later)");
     }
 }
--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -4,6 +4,9 @@ package com.example.demo.security;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
 public class SecurityConfig {
@@ -22,6 +25,13 @@ public class SecurityConfig {
             // Lägg till oauth2Login()
         ;
 
+
         return http.build();
     }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
 }
