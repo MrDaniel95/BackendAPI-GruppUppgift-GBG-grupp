ee0e6e980080a106f45248fef3b880181190e434
Daniel B <158176937+Mrdbs95@users.noreply.github.com>
2026-05-06 15:24:52 +0200

    Tar tillbaka koden innan "Fix"

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.demo.repo.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.*;
@@ -32,6 +33,8 @@ public class SecurityConfig {
                 )
 
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+
+                .oauth2Login(Customizer.withDefaults())
 
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 
